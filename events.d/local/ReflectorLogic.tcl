9ebf1b105a74a1963f504944dd3b5e120ff6df48
DanZ SP0DZ <zochowsd@gmail.com>
2023-11-13 22:20:39 +0100

    uncommented changes in Reflector logic for talker stop

--- events.d/local/ReflectorLogic.tcl
+++ events.d/local/ReflectorLogic.tcl
@@ -388,15 +388,16 @@ proc talker_start {tg callsign} {
 #   callsign  -- The callsign of the talker node
 #
 proc talker_stop {tg callsign} {
-  #variable selected_tg
-  #variable ::Logic::CFG_CALLSIGN
-  #puts "### Talker stop on TG #$tg: $callsign"
-  #if {($tg == $selected_tg) && ($callsign != $::Logic::CFG_CALLSIGN)} {
-  #  playSilence 100
-  #  playTone 440 200 50
+  variable selected_tg
+  variable ::Logic::CFG_CALLSIGN
+  puts "### Talker stop on TG #$tg: $callsign"
+  if {($tg == $selected_tg) && ($callsign != $::Logic::CFG_CALLSIGN)} {
+    playSilence 100
+    playTone 440 200 50
   #  playTone 659 200 50
   #  playTone 880 200 50
-  #}
+  playSilence 10
+  }
 }
 
 
